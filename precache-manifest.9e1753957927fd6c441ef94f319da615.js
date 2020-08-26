c94ed45a62f9b559d6cfc761205ddecb26795219
cool1997 <kirilov.vl.ol@gmail.com>
2020-08-26 18:37:47 +0300

    Updates

---
+++ precache-manifest.9e1753957927fd6c441ef94f319da615.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4a85b0b0579dbe0c3beba9d3c93c21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dafb8d2bebceb1daaf3",
+    "url": "/static/css/main.18496959.chunk.css"
+  },
+  {
+    "revision": "bf8912b5b3a724f15f02",
+    "url": "/static/js/2.9128de41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9128de41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dafb8d2bebceb1daaf3",
+    "url": "/static/js/main.6291aa8f.chunk.js"
+  },
+  {
+    "revision": "3c8779638c1df76c4975",
+    "url": "/static/js/runtime-main.87887fdf.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "36fdd8e3447b5dc0d01605324d56fb8f",
+    "url": "/static/media/funbox_cat-logo.36fdd8e3.png"
+  }
+]);
