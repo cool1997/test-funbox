446bdddfcd79d5ebec588192b15e7b1ffa344075
cool1997 <kirilov.vl.ol@gmail.com>
2020-08-26 18:53:50 +0300

    Updates

---
+++ precache-manifest.6f78a128df932513f961a4a6482e84e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da95d48f3aeeaf8e20d3c92c11e6aab",
+    "url": "/test-funbox/index.html"
+  },
+  {
+    "revision": "0ef034b2fde2c7ae3a1c",
+    "url": "/test-funbox/static/css/main.b8a19316.chunk.css"
+  },
+  {
+    "revision": "bf8912b5b3a724f15f02",
+    "url": "/test-funbox/static/js/2.9128de41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-funbox/static/js/2.9128de41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef034b2fde2c7ae3a1c",
+    "url": "/test-funbox/static/js/main.6291aa8f.chunk.js"
+  },
+  {
+    "revision": "7f3f8ed2c5d7bbc26477",
+    "url": "/test-funbox/static/js/runtime-main.2fc311a1.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/test-funbox/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "36fdd8e3447b5dc0d01605324d56fb8f",
+    "url": "/test-funbox/static/media/funbox_cat-logo.36fdd8e3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e1753957927fd6c441ef94f319da615.js"
+  "/test-funbox/precache-manifest.6f78a128df932513f961a4a6482e84e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-funbox/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
